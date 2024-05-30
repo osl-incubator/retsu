9bd428b18b7ab7e7ee72c07de89668b6f8e4ed5e
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2024-05-30 13:53:56 -0400

    fix tests workflow

--- tests/test_task_serial.py
+++ tests/test_task_serial.py
@@ -10,34 +10,29 @@ class MyTask(SerialTask):
         return a + b
 
 
-class TestManager:
-    # @classmethod
-    # def class_setup(cls) -> None:
-    #     self.task_manager = TaskManager()
+class SetupTask:
+    @classmethod
+    def setup_class(cls) -> None:
+        cls.task = MyTask()
+        cls.task.start()
 
-    # @classmethod
-    # def class_teardown(cls) -> None:
-    #     self.task_manager.stop
-    ...
+    @classmethod
+    def teardown_class(cls) -> None:
+        cls.task.stop()
 
 
-class TestSerialTask:
+class TestSerialTask(SetupTask):
     """TestSerialTask."""
 
     def test_serial(self):
-        task = MyTask()
-        task.start()
-
         results: dict[str, int] = {}
 
         for i in range(10):
-            task_id = task.request(a=i, b=i)
+            task_id = self.task.request(a=i, b=i)
             results[task_id] = i + i
 
         for task_id, expected in results.items():
-            result = task.result.get(task_id, timeout=2)
+            result = self.task.result.get(task_id, timeout=2)
             assert (
                 result == expected
             ), f"Expected Result: {expected}, Actual Result: {result}"
-
-        task.stop()
